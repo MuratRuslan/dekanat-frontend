9c6df8a0bff4a4f971c796c0bc3bdedee251fcaf
PlusRT <temirlan.renatov@gmail.com>
2017-11-07 22:18:11 +0600

    doing services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GroupListComponent } from './group-list/group-list.component';
+import { GroupListComponent } from './drop-down-list/group-list/group-list.component';
 import { GroupTimetableComponent } from './group-timetable/group-timetable.component';
 import { WeekdayTimetableComponent } from './weekday-timetable/weekday-timetable.component';
 import { WeekdayListComponent } from './weekday-list/weekday-list.component';
 import {AppRoutingModule} from './app-routing.module';
 import {FormsModule} from '@angular/forms';
 import { LessonComponent } from './lesson/lesson.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TeachersListComponent } from './drop-down-list/teachers-list/teachers-list.component';
+import {TeacherService} from './service/teacher-service';
+import { AddTeacherComponent } from './add-new/add-teacher/add-teacher.component';
 
 @NgModule({
   declarations: [
@@ -17,14 +21,17 @@ import { LessonComponent } from './lesson/lesson.component';
     GroupTimetableComponent,
     WeekdayTimetableComponent,
     WeekdayListComponent,
-    LessonComponent
+    LessonComponent,
+    TeachersListComponent,
+    AddTeacherComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TeacherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
