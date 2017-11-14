7bc5217f7eb228f5338b1330996237f366e11027
PlusRT <temirlan.renatov@gmail.com>
2017-11-14 21:48:27 +0600

    ...

--- src/app/journal-module/group/group-info/group-info.component.ts
+++ src/app/journal-module/group/group-info/group-info.component.ts
@@ -17,6 +17,8 @@ import {SemesterService} from '../../../service/semester-service';
 })
 export class GroupInfoComponent implements OnInit, OnDestroy {
 
+  tempSubjects: Subject[] = [{id: 5, name: 'Database', teachers: []},
+    {id: 1, name: 'Algorithmen', teachers: []}];
   group: Gruppa = new Gruppa();
   allSubjectsInDekanat: Subject[];
   subjects: Subject[] = [];
